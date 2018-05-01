d9ee6846102371e9dfcc9d6129f8346b714fc67f
Glitch (fcc-urlshorten) <none>
2018-05-01 22:04:35 +0000

    👲🍔 Checkpoint ./server.js:1015202/76

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ app.get('/', function(req, res){
   
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
+  // res.redirect("http://freecodecamp.org");
   res.json({greeting: 'hello API'});
 });
 
