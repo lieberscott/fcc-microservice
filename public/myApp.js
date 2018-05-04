747aafc8f31d7c3215543faa3b3bfc670a2a3c54
Glitch (fcc-urlshorten) <none>
2018-05-04 23:47:55 +0000

    💃😘 Checkpoint ./public/myApp.js:1015202/3

--- public/myApp.js
+++ public/myApp.js
@@ -1,6 +1,6 @@
 console.log("hello1");
 
-// let mongoose = require("mongoose");
+let mongoose = require("mongoose");
 // mongoose.connect(process.env.MONGO_URI);
 
 // let Schema = mongoose.Schema;
