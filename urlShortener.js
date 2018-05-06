93dc2d2bb0bcbbc4d8ab147c6679cb6b4394c35c
Glitch (fcc-urlshorten) <none>
2018-05-06 09:29:35 +0000

    👨‍❤️‍💋‍👨📟 Checkpoint ./urlShortener.js:1015202/1378

--- urlShortener.js
+++ urlShortener.js
@@ -5,12 +5,7 @@ console.log("hello1");
 let mongoose = require("mongoose");
 // mongoose.connect(process.env.MONGO_URI);
 
-// let cors = require('cors');
 let dns = require('dns');
-// let express = require('express');
-// let app = express();
-// let bodyParser = require('body-parser');
-// app.use(bodyParser.urlencoded({ extended: true })); // middleware to capture the input field of a form
 
 let Schema = mongoose.Schema;
 
@@ -24,20 +19,10 @@ let UrlSchema = new Schema({
 
 let Url = mongoose.model("Url", UrlSchema);
 
-let url = req.body.url; // captures input field of form; "url" here matches <input name="url"> in index.html file
-
-exports.createShort = function(req, res) {
-  let getLength = (done) => {
-    let len;
-    Url.count({}, (err, data) => {
-      if (err) { done(err) }
-      else {
-        len = data;
-        done(null, data);
-      }
-    });
-  }
-}
+let createShort = function(req, res) {
+  let url = req.body.url; // captures input field of form; "url" here matches <input name="url"> in index.html file
+  let len = Url.count({});
+  len.then(num => len = num);
   console.log(len);
   let last = Url.findOne({ short: 0 }, "short", function(err, data) {
     if (err) {
@@ -54,17 +39,7 @@ exports.createShort = function(req, res) {
     }
     else { console.log("no error") }
   });
-  // console.log(last);
 
-//   let short = new Url({
-//   long: url,
-//   short: last + 1
-// });
-  
-//   short.save((err, data) => {
-//     if (err) { console.log(err) }
-//     else { console.log(data) }
-//   });
   
   res.json({hello: url});
 };
@@ -87,4 +62,4 @@ exports.createShort = function(req, res) {
 //----------- Do not edit below this line -----------//
 
 exports.UrlModel = Url;
-// exports.createShort = createShort;
+exports.createShort = createShort;
