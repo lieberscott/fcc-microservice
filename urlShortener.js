d6aa683383e98d7ce9d2f161311a5bdb1cdb9ed4
Glitch (fcc-urlshorten) <none>
2018-05-06 10:49:09 +0000

    ⛈🏡 Checkpoint ./urlShortener.js:1015202/3072

--- urlShortener.js
+++ urlShortener.js
@@ -21,48 +21,42 @@ let Url = mongoose.model("Url", UrlSchema);
 
 let createShort = function(req, res) {
   let url = req.body.url; // captures input field of form; "url" here matches <input name="url"> in index.html file
-  let len;
   async function dbOperations() {
-    // let len;
+    let len;
     len = await Url.count({}).exec();
-    // console.log(len);
-}
-  dbOperations();
-  console.log(len);
-//   let len = Url.count({});
-//   len.then(num => len = num);
-//   console.log(len);
-//   let last = Url.findOne({ short: 0 }, "short", function(err, data) {
-//     if (err) {
-//       let short = new Url({
-//         long: url,
-//         short: 1
-//       });
+    console.log("len: " + len);
+    let newshort;
+    
+    
+    if (len == 0) {
+      newshort = new Url({
+        long: url,
+        short: 1
+      });
       
-//       short.save((err, data) => {
-//         if (err) { console.log("saving error") }
-//         else { console.log("saving success") }
-//       });
-//       console.log("first entry");
-//     }
-//     else { console.log("no error") }
-//   });
-
+      newshort.save((err, data) => {
+        if (err) { console.log(err) }
+        else { console.log(data) }
+      });
+    }
+    
+    else {
+      newshort = new Url({
+        long: url,
+        short: len + 1
+      });
+      
+      newshort.save((err, data) => {
+        if (err) { console.log(err) }
+        else { console.log(data) }
+      });
+    }
+  }
+  
+  dbOperations();
   
   res.json({hello: url});
 };
-
-
-// let queryChain = function(done) {
-//   let foodToSearch = "burrito";
-  
-//   let burritoLovers = Person.find({favoriteFoods: foodToSearch});
-  
-//   burritoLovers.sort({ name: "asc" }).limit(2).select("-age").exec((err, data) => {
-//     if (err) { done(err) }
-//     else { done(null, data) }
-//   });
-// };
 
 
 
